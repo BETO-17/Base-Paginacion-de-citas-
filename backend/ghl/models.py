89f4455c64bc190088d141ce0da49f8724d4e74e
Franco <francodeniro.24@gmail.com>
2025-09-14 21:46:33 -0500

    Completo

--- backend/ghl/models.py
+++ backend/ghl/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    ghl_id = models.CharField(max_length=100, unique=True)  # ID que devuelve GHL
+    calendar_id = models.CharField(max_length=100)
+    contact_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title or 'Sin título'} ({self.ghl_id})"
+
--- backend/ghl/urls.py
+++ backend/ghl/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import CalendarListView, AppointmentCreateView
+from .views import (
+    CalendarListView,
+    AppointmentCreateView,
+    AppointmentDetailView,
+    BatchAppointmentsView,
+   appointment_list,
+   AllGHLAppointmentsView
+
+)
 
 urlpatterns = [
     path("calendars/", CalendarListView.as_view(), name="ghl_calendars"),
     path("ghl/appointment/", AppointmentCreateView.as_view(), name="ghl_appointment"),
+    path("ghl/appointments/<str:event_id>/", AppointmentDetailView.as_view(), name="ghl_appointment_detail"),
+    path("ghl/appointments/batch/", BatchAppointmentsView.as_view(), name="batch_appointments"),
+    path('api/appointments/', appointment_list, name='appointment_list'),
+    path('api/ghl-appointments/', AllGHLAppointmentsView.as_view(), name='all_ghl_appointments')
+
 ]
